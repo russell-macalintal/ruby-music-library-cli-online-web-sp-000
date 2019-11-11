85e039129b7c49389e58b301f98104de672fa5b2
Russell Macalintal <russell.macalintal@gmail.com>
2019-11-11 03:17:40 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -16,13 +16,10 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    input = gets.strip
-    binding.pry
-    # until input == 'exit'
+    # input = gets.strip
+    # until input == 'toodoo'
     #   call
-    #   input
     # end
-    # "exit"
   end
 
 end
