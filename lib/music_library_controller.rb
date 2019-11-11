264ccee46e4af119c73be14317a48d8642b41abf
Russell Macalintal <russell.macalintal@gmail.com>
2019-11-11 04:36:56 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -69,7 +69,7 @@ class MusicLibraryController
     end
 
     def play_song
-      self.list_songs
+      # self.list_songs
       puts "Which song number would you like to play?"
       input = gets.strip
       integer = input.to_i
